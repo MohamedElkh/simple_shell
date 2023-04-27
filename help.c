f1312278591504a3417db5f422f61c260c7a214e
Hlogimaepa <hlogimaepa80@gmail.com>
2023-04-27 14:30:52 +0200

    Update help.c

--- help.c
+++ help.c
@@ -1,108 +1,151 @@
 #include "shell.h"
 
+void free_args(char **args, char **front);
+char *get_pid(void);
+char *get_env_value(char *beginning, int len);
+void variable_replacement(char **args, int *exe_ret);
+
 /**
- * _strlen - function to count the length of str
- * @str: the character to be checked
- *
- * Return: the result.
+ * free_args - Frees up memory taken by args.
+ * @args: A null-terminated double pointer containing commands/arguments.
+ * @front: A double pointer to the beginning of args.
  */
-
-int _strlen(char *str)
+void free_args(char **args, char **front)
 {
-	int x = 0;
+	size_t i;
 
-	for (x = 0 ; str[x] != '\0'; x++)
+	for (i = 0; args[i] || args[i + 1]; i++)
+		free(args[i]);
+
+	free(front);
+}
+
+/**
+ * Description: Opens the stat file, a space-delimited file containing
+ *              information about the current process.
+ * Return: The current process ID or NULL on failure.
+ */
+char *get_pid(void)
+{
+	size_t i = 0;
+	char *buffer;
+	ssize_t file;
+
+	file = open("/proc/self/stat", O_RDONLY);
+	if (file == -1)
 	{
-		continue;
+		perror("Cant read file");
+		return (NULL);
 	}
-	return (x);
-}
-
-/**
- * _putchar - write the character c to stdout
- * @c: the character to be checked
- *
- * Return: the result.
- */
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * _puts - function to print the string
- * @s: the character to be checked
- *
- * Return: nothing.
- */
-
-void _puts(char *s)
-{
-	int x;
-
-	for (x = 0; s[x] != '\0'; x++)
+	buffer = malloc(120);
+	if (!buffer)
 	{
-		_putchar(s[x]);
+		close(file);
+		return (NULL);
 	}
-	_putchar('\n');
-	return;
+	read(file, buffer, 120);
+	while (buffer[i] != ' ')
+		i++;
+	buffer[i] = '\0';
+
+	close(file);
+	return (buffer);
 }
 
 /**
- * _atoi - function to convert to intigar
- * @str: string
+ * get_env_value - Gets the value corresponding to an environmental variable.
+ * @beginning: The environmental variable to search for.
+ * @len: The length of the environmental variable to search for.
  *
- * Return: the result.
+ * Return: If the variable is not found - an empty string.
+ *         Otherwise - the value of the environmental variable.
+ *
+ * Description: Variables are stored in the format VARIABLE=VALUE.
  */
-
-int _atoi(char *str)
+char *get_env_value(char *beginning, int len)
 {
-	int a, nu, x, i;
-	a = nu = 0;
-	i = 1;
+	char **var_addr;
+	char *replacement = NULL, *temp, *var;
 
-	while ((str[a] < '0' || str[a] > '9') && (str[a] != '\0'))
+	var = malloc(len + 1);
+	if (!var)
+		return (NULL);
+	var[0] = '\0';
+	_strncat(var, beginning, len);
+
+	var_addr = _getenv(var);
+	free(var);
+	if (var_addr)
 	{
-		if (str[a] == '-')
+		temp = *var_addr;
+		while (*temp != '=')
+			temp++;
+		temp++;
+		replacement = malloc(_strlen(temp) + 1);
+		if (replacement)
+			_strcpy(replacement, temp);
+	}
+
+	return (replacement);
+}
+
+/**
+ * variable_replacement - Handles variable replacement.
+ * @line: A double pointer containing the command and arguments.
+ * @exe_ret: A pointer to the return value of the last executed command.
+ *
+ * Description: Replaces $$ with the current PID, $? with the return value
+ *              of the last executed program, and envrionmental variables
+ *              preceded by $ with their corresponding value.
+ */
+void variable_replacement(char **line, int *exe_ret)
+{
+	int j, k = 0, len;
+	char *replacement = NULL, *old_line = NULL, *new_line;
+
+	old_line = *line;
+	for (j = 0; old_line[j]; j++)
+	{
+		if (old_line[j] == '$' && old_line[j + 1] &&
+				old_line[j + 1] != ' ')
 		{
-			i *= -1;
+			if (old_line[j + 1] == '$')
+			{
+				replacement = get_pid();
+				k = j + 2;
+			}
+			else if (old_line[j + 1] == '?')
+			{
+				replacement = _itoa(*exe_ret);
+				k = j + 2;
+			}
+			else if (old_line[j + 1])
+			{
+				/* extract the variable name to search for */
+				for (k = j + 1; old_line[k] &&
+						old_line[k] != '$' &&
+						old_line[k] != ' '; k++)
+					;
+				len = k - (j + 1);
+				replacement = get_env_value(&old_line[j + 1], len);
+			}
+			new_line = malloc(j + _strlen(replacement)
+					  + _strlen(&old_line[k]) + 1);
+			if (!line)
+				return;
+			new_line[0] = '\0';
+			_strncat(new_line, old_line, j);
+			if (replacement)
+			{
+				_strcat(new_line, replacement);
+				free(replacement);
+				replacement = NULL;
+			}
+			_strcat(new_line, &old_line[k]);
+			free(old_line);
+			*line = new_line;
+			old_line = new_line;
+			j = -1;
 		}
-		a++;
 	}
-	x = a;
-
-	while ((str[x] >= '0') && (str[x] <= '9'))
-	{
-		nu = (nu * 10) + i * ((str[x]) - '0');
-		x++;
-	}
-	return (nu);
-
-}
-
-/**
- * _strncpy - function to copy the string
- * @desti: the character to be checked
- * @src: the character to be checked
- * @num: the number to be checked
- *
- * Return: the result.
- */
-
-char *_strncpy(char *desti, char *src, int num)
-{
-	int x = 0;
-
-	while (x < num && *(src + x))
-	{
-		*(desti + x) = *(src + x);
-		x++;
-	}
-	while (x < num)
-	{
-		*(desti + x) = '\0';
-		x++;
-	}
-	return (desti);
 }
